df7dbdf1617ebf62ab3662febab8f51a559d1572
Silvio Braendle <silvio@silviobraendle.ch>
2025-04-22 18:54:48 +0200

    Pass roles to session

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -7,12 +7,26 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     Keycloak({
       authorization: {
         params: {
-          scope: "openid profile email",
+          scope: "openid profile email with_roles",
         },
       },
     }),
   ],
   callbacks: {
+    async jwt({ token, account, profile }) {
+      console.log("jwt", token, account);
+      if (profile?.roles) {
+        token.roles = profile.roles;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      console.log("session", session, token);
+      if (token?.roles) {
+        session.roles = token.roles;
+      }
+      return session;
+    },
     async authorized({ auth }) {
       console.log("authorized", auth);
       return true;
